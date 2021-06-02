6684c0895da886f0137242568c40e0dad5ee7b1d
Vijay Gorfad <gorfadvijay1234@gmail.com>
2021-06-02 12:52:33 +0530

    Merge branch 'master' of https://github.com/gorfadumesh/gorfadumesh

--- umeshgorfad/gatsby-config.js
+++ umeshgorfad/gatsby-config.js
@@ -16,7 +16,7 @@ module.exports = {
         background_color: `#111a28`,
         theme_color: `#1d293a`,
         display: `minimal-ui`,
-        icon: `src/assets/image/favicon.png`, // This path is relative to the root of the site.
+        icon: `src/assets/image/favicon2.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
